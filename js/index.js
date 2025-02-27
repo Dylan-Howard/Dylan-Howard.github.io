a7a2c1a6a502bc87a6a4c1754e4e619aa98707b2
Dylan Howard <dylanhoward6@gmail.com>
2025-02-27 08:31:07 -0600

    Addded interest form

--- js/index.js
+++ js/index.js
@@ -23,3 +23,31 @@ const toggleSwitch = () => {
 
 const themeSwitch = document.getElementById('color-mode-switch');
 themeSwitch.addEventListener('change', toggleSwitch);
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById('interest-form-submit').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent default form submission
+  
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+  
+    // Send the data to your Google Apps Script Web App
+    fetch("https://script.google.com/macros/s/AKfycbzgcp7xdbWVFjB3CZHm0G_mTBTWR0Ma-x-KPhTETEYd_2x9E9GgII6S79eeeTBsMxQX_g/exec", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: name, email: email }),
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.result === 'success') {
+        document.getElementById('interest-form-submit').reset(); //Clear the form
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert('An unexpected error occurred.');
+    });
+  });
+});
