c0316ece53abed208862305c383e89c11c3f852d
Dylan Howard <dylanhoward6@gmail.com>
2025-04-23 10:43:21 -0500

    Linted files with CSS-order

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
+  compressHTML: true,
   site: "https://dylan-howard.github.io",
-  integrations: [sitemap()],
+  integrations: [compress(), sitemap()],
 });
